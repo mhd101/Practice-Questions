eab7904b492bcb9672db87cef4db45df8fd9c95d
mhd101 <itzmohammadkhan@gmail.com>
2025-07-02 05:58:54 +0000

    Factorial

---
+++ Math/src/Factorial.java
@@ -0,0 +1,27 @@
+/* Given a positive integer, n. Find the factorial of n.
+
+Input: n = 5
+Output: 120
+Explanation: 1 x 2 x 3 x 4 x 5 = 120
+
+Constraints:
+0 <= n <= 12
+
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+*/
+
+public class Factorial {
+    static int factorial(int n){
+        if (n == 0){
+            return 1;
+        }
+
+        return n * factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.factorial(5));
+    }
+}
